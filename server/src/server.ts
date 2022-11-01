8d6dc7b7b2089700373b90cff3a402445e722d0b
José Ronaldo <dev.joseronaldo@gmail.com>
2022-10-31 21:31:13 -0300

    feat: aula 1 - setup backend

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true,
+    // em produção colocar o domínio ex:"https://www.bolaojoseronaldo.vercel.app"
+  })
+
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.poll.count()
+
+    return { count }  
+  })
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
+}
+
+bootstrap()
